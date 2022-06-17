0506d2bd63d3e726b110caf03507563f0628de62
iioter <535915157@qq.com>
2022-06-17 16:44:36 +0800

    ini

---
+++ src/NLog.Demo/Program.cs
@@ -0,0 +1,48 @@
+using NLog;
+using NLog.Web;
+
+var logger = NLog.LogManager.Setup().LoadConfigurationFromAppSettings().GetCurrentClassLogger();
+logger.Debug("init main");
+
+try
+{
+    var builder = WebApplication.CreateBuilder(args);
+
+    // Add services to the container.
+
+    builder.Services.AddControllers();
+    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+
+    // NLog: Setup NLog for Dependency injection
+    builder.Logging.ClearProviders();
+    builder.Logging.SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.Trace);
+    builder.Host.UseNLog();
+
+    var app = builder.Build();
+
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    app.UseAuthorization();
+
+    app.MapControllers();
+
+    app.Run();
+}
+catch (Exception e)
+{
+    // NLog: catch setup errors
+    logger.Error(e, "Stopped program because of exception");
+    throw;
+}
+finally
+{
+    // Ensure to flush and stop internal timers/threads before application-exit (Avoid segmentation fault on Linux)
+    NLog.LogManager.Shutdown();
+}
---
+++ src/NLog.Targets.MQTT/MqttTarget.cs
@@ -0,0 +1,98 @@
+﻿using MQTTnet;
+using MQTTnet.Client;
+using MQTTnet.Client.Disconnecting;
+using MQTTnet.Client.Options;
+using NLog.Config;
+
+namespace NLog.Targets.MQTT
+{
+    [Target("Mqtt")]
+    public class MqttTarget : TargetWithLayout
+    {
+        private IMqttClient mqttClient;
+        private IMqttClientOptions mqttClientOptions;
+        public MqttTarget()
+        {
+            Host = "localhost";
+            Port = 1883;
+            Topic = "iotgateway/log/";
+        }
+
+        #region RequiredParameters
+        /// <summary>
+        /// Broker Host
+        /// </summary>
+        [RequiredParameter]
+        public string Host { get; set; }
+
+        /// <summary>
+        /// Broker Port
+        /// </summary>
+        [RequiredParameter]
+        public int Port { get; set; }
+
+        /// <summary>
+        /// UserName
+        /// </summary>
+        [RequiredParameter]
+        public string UserName { get; set; }
+
+        /// <summary>
+        /// Password
+        /// </summary>
+        [RequiredParameter]
+        public string Password { get; set; }
+
+        public string Topic { get; set; }
+        #endregion
+
+        protected override void Write(LogEventInfo logEvent)
+        {
+            string logMessage = RenderLogEvent(this.Layout, logEvent);
+            string hostName = RenderLogEvent(Host, logEvent);
+            SendTheMessageToRemoteHost(hostName, logMessage);
+        }
+
+        private void SendTheMessageToRemoteHost(string hostName, string message)
+        {
+            var applicationMessage = new MqttApplicationMessageBuilder()
+                .WithTopic(Topic)
+                .WithPayload(message)
+                .Build();
+            mqttClient.PublishAsync(applicationMessage, CancellationToken.None);
+        }
+
+        protected override void InitializeTarget()
+        {
+            base.InitializeTarget();
+
+            mqttClient = new MqttFactory().CreateMqttClient();
+            mqttClientOptions = new MqttClientOptionsBuilder()
+                .WithClientId("NLog.Targets.MQTT")
+                .WithTcpServer(Host, Port)
+                .WithCredentials(UserName, Password)
+                .Build();
+
+            mqttClient.DisconnectedHandler = new MqttClientDisconnectedHandlerDelegate(x => OnDisconnectedAsync());
+            mqttClient.ConnectAsync(mqttClientOptions, CancellationToken.None);
+        }
+
+        protected override void CloseTarget()
+        {
+            base.CloseTarget();
+            mqttClient.DisconnectAsync(CancellationToken.None);
+        }
+
+        private async Task OnDisconnectedAsync()
+        {
+            try
+            {
+                await mqttClient.ConnectAsync(mqttClientOptions);
+            }
+            catch (Exception exception)
+            {
+                //"MQTT CONNECTING FAILED
+            }
+        }
+    }
+}
